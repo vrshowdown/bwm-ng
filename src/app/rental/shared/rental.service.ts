f456b2c2ec6d58e9b189ac2ca0524ce7d20b89bc
vrshowdown <jmu3dx@gmail.com>
2019-03-06 15:36:29 -0500

    Add proxy configuration, adds rental details styles improvements, and adds pipes

--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -8,92 +8,27 @@ import {Observable} from 'rxjs'; // declares Observables from Reactive Extension
 
 import { Rental } from './rental.model'; // for rental type
 
+import {HttpClient} from '@angular/common/http';
  @Injectable()
  export class RentalService{
 //the array of object data  for Rental Service
 // changed from private rentals: any[] =[{  
-private rentals: Rental[] =[   
-             {
-            id: "1", //ids are changed to a string  for single page address 
-            title: "Central Apartment",
-            city: "New York",
-            street: "Times Square",
-            category: "apartment",
-            image: "http://via.placeholder.com/350x250",
-            bedrooms: 2,
-            description: "Very nice apartment", 
-            dailyRate: 34,
-            shared: false,
-            createdAt: "24/12/2017"
-            },
-            {
-              id: "2",  
-              title: "Central Apartment 2",
-              city: "San Francisco",
-              street: "Main street",
-              category: "condo",
-              image: "http://via.placeholder.com/350x250",
-              bedrooms: 3,
-              description: "Very Nice Condo", 
-              dailyRate: 25,
-              shared: true,
-              createdAt: "24/12/2017"
-            },
-            {
-              id: "3",  
-              title: "Central Apartment 3",
-              city: "Bratislava",
-              street: "Hlavna",
-              category: "condo",
-              image: "http://via.placeholder.com/350x250",
-              bedrooms: 3, 
-              description: "Very Nice Condo",
-              dailyRate: 334,
-              shared: true,
-              createdAt: "24/12/2017"
-            },
-            {
-              id: "4",  
-              title: "Central Apartment",
-              city: "Berlin",
-              street: "Haupt strasse",
-              category: "house",
-              image: "http://via.placeholder.com/350x250",
-              bedrooms: 3,
-              description: "Very Nice House", 
-              dailyRate: 15,
-              shared: true,
-              createdAt: "24/12/2017"
-            }, 
-
-    ];
-
+// removed private rentals: Rental[] = [{ all data }];
+constructor(private http: HttpClient){}
 //Make observable function for data ids,    will look up id and compare it to the id in the array
-public getRentalById(rentalId: string): Observable<Rental>{
-  return new Observable<Rental>((observer)=>{
-    setTimeout(()=>{
-      const foundRental = this.rentals.find((rental)=>{ 
-        return rental.id == rentalId; //if rental.id == rentalid,  return true
-      });
-      observer.next(foundRental);  // emit found data  from service
-    }, 500); 
-  });
+public getRentalById(rentalId: string): Observable<any>{
+  // changed observer function to this
+  return   this.http.get('/api/v1/rentals/' + rentalId);     
 }
 
 
 // changed from getRentals(): any{
-    public  getRentals(): Observable<Rental[]>{
+    public  getRentals(): Observable<any>{
 //while recieving rental data  asynchronously, rentalObservable  Manages  stream of data  before displaying data
      //1. changed from const rentalObservable = new Observable((observer)=>{ 
      //2. Changed from const rentalObservable: Observable<Rental[]>= new Observable((observer)=>{ 
-      return new Observable<Rental[]>((observer)=>{ 
-        setTimeout(()=>{
-          observer.next(this.rentals);
-        }, 1000); 
-        //observer.error & observer.complete
-      });
-      // return rentalObservable;  was removed
- 
+     //3. changed from  return new Observable<Rental[]>((observer)=>{all of the functions});
+     return   this.http.get('/api/v1/rentals');
     }
 
 }
