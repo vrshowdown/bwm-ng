cce24f0d822de7b441f0a0403a32e6faf04c9ebb
Jibreel <jmu3dx@gmail.com>
2026-01-08 10:58:40 +0800

    Replace SendGrid with Brevo email API

--- server/config/prod.js
+++ server/config/prod.js
@@ -6,5 +6,6 @@ module.exports = {
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     STRIPE_SK: process.env.STRIPE_SK,
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    DOMAIN: process.env.DOMAIN
+    DOMAIN: process.env.DOMAIN,
+    BREVO_API_KEY: process.env.BREVO_API_KEY,
 }
--- server/controllers/userp.js
+++ server/controllers/userp.js
@@ -6,56 +6,57 @@ const User = require('../models/user');
 const { normalizeErrors } = require('../helpers/mongoose');
 //const jwt = require('jsonwebtoken');
 const config = require('../config');
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(config.SENDGRID_API_KEY);
+const SibApiV3Sdk = require('@getbrevo/brevo');
+let brevoApiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+brevoApiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, config.BREVO_API_KEY);
 
 
 
 
 //to get user detail by id and display Public user rentals
-exports.getUserP = function(req, res){
+exports.getUserP = function (req, res) {
 
-    const userId = req.params.id;                                    
+    const userId = req.params.id;
     UserP.findById(userId)
-    .populate({
-        path: 'user', //in userp locate user with its id and populate
-        select: 'rentals image-_id username-_id about-_id', //include rentals, image, username, and about. Leave out/exclude user id
-        populate: { //in rentals populate  array with
-            path: 'rentals',
-            model: 'Rental',
-           select: 'category image shared city title dailyRate _id' //include category, image,shared, city, title dailyrate and rental id
-          }
-    })  
-    .exec(function(err, foundUser){
-        
-     if (err){
-           return res.status(422).send({errors:[{title: 'User Error!', detail: 'Could not find User!'}]});   
-        }
-       //req.locals.id = foundUser.id;
-        return res.json(foundUser); 
-    });
+        .populate({
+            path: 'user', //in userp locate user with its id and populate
+            select: 'rentals image-_id username-_id about-_id', //include rentals, image, username, and about. Leave out/exclude user id
+            populate: { //in rentals populate  array with
+                path: 'rentals',
+                model: 'Rental',
+                select: 'category image shared city title dailyRate _id' //include category, image,shared, city, title dailyrate and rental id
+            }
+        })
+        .exec(function (err, foundUser) {
+
+            if (err) {
+                return res.status(422).send({ errors: [{ title: 'User Error!', detail: 'Could not find User!' }] });
+            }
+            //req.locals.id = foundUser.id;
+            return res.json(foundUser);
+        });
 }
 
 
 
 // to get public id 
 
-exports.getUserP2 = function(req, res){
-   
-    const user =  res.locals.user;
-    
-       
-        UserP.findOne({user}, function(err, foundUserPId){
-            if (err){
-                return res.status(422).send({errors: normalizeErrors(err.errors)});
-            }
+exports.getUserP2 = function (req, res) {
+
+    const user = res.locals.user;
 
 
-          
-            return res.json(foundUserPId.id);   
+    UserP.findOne({ user }, function (err, foundUserPId) {
+        if (err) {
+            return res.status(422).send({ errors: normalizeErrors(err.errors) });
+        }
+
+
+
+        return res.json(foundUserPId.id);
     });
 
 
-//return res.json('You did it');
-//return res.json(user);
+    //return res.json('You did it');
+    //return res.json(user);
 }
